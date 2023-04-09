e4a04951ca9ed1c68e0be23b82a5526d0b182aa7
patchwork-body <personal.gugfug@gmail.com>
2023-04-09 21:41:10 +0200

    feat: initiate Prisma client according to the env
    according to this issue: https://github.com/prisma/prisma/issues/6219
    we should differentiate how we initializing the Prisma client instance

--- src/lib/prisma/client.ts
+++ src/lib/prisma/client.ts
@@ -1,3 +1,19 @@
+/**
+ * Instantiates a single instance PrismaClient and save it on the global object.
+ * @link https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
+ */
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const prismaGlobal = global as typeof global & {
+  prisma?: PrismaClient;
+};
+
+export const prisma: PrismaClient =
+  prismaGlobal.prisma ||
+  new PrismaClient({
+    log: import.meta.env.DEV ? ['query', 'error', 'warn'] : ['error'],
+  });
+
+if (import.meta.env.PROD) {
+  prismaGlobal.prisma = prisma;
+}
