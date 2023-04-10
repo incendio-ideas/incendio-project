f175473d9fed75e73a23b3062b886ea8a2b22967
patchwork-body <personal.gugfug@gmail.com>
2023-04-10 12:15:16 +0200

    fix(prisma): use data proxy client
    example: https://github.com/ruheni/prisma-edge-functions/blob/main/lib/prisma.ts
    article: https://www.prisma.io/docs/data-platform/data-proxy
    doc: https://www.prisma.io/blog/database-access-on-the-edge-8F0t1s1BqOJE

--- src/lib/prisma/client.ts
+++ src/lib/prisma/client.ts
@@ -2,23 +2,18 @@
  * Instantiates a single instance PrismaClient and save it on the global object.
  * @link https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
  */
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 
 const prismaGlobal = global as typeof global & {
   prisma?: PrismaClient;
 };
 
-const isServer = typeof window === 'undefined';
-export let prisma: PrismaClient;
+export const prisma =
+  prismaGlobal.prisma ||
+  new PrismaClient({
+    log: import.meta.env.DEV ? ['query', 'error', 'warn'] : ['error'],
+  });
 
-if (isServer) {
-  prisma =
-    prismaGlobal.prisma ||
-    new PrismaClient({
-      log: import.meta.env.DEV ? ['query', 'error', 'warn'] : ['error'],
-    });
-
-  if (import.meta.env.PROD) {
-    prismaGlobal.prisma = prisma;
-  }
+if (import.meta.env.PROD) {
+  prismaGlobal.prisma = prisma;
 }
