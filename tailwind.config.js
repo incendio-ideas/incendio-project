3e156d646bc18cac02d1c9d5c435191bda61a30c
patchwork-body <personal.gugfug@gmail.com>
2023-05-07 21:35:06 +0200

    feat: add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: 'jit',
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
